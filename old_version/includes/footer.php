e406da9e1848c8fa0b97c3d46885585d6656bdaa
Анисимов Кирилл <k.anisimov@maximaster.ru>
2015-10-31 17:48:39 +0300

    Залил сайт

---
+++ old_version/includes/footer.php
@@ -0,0 +1,27 @@
+	<div class="row footer">
+		<div class="span12">
+			<div class="data">
+			    <h4>Email:</h4>
+			    <i class="icon-envelope"></i> <a href="mailto:akpoflash@gmail.com">akpoflash@gmail.com</a>
+		    </div>
+		    <div class="data">
+		    	<h4>Телефон:</h4>
+		    	<i class="icon-bell"></i> <a href="tel:+7 920 276 29 50">+7 920 276 29 50</a>
+		    </div>
+		    <div class="data">
+		    	<h4>Сайт разработан:</h4>
+		    	<i class="icon-globe"></i> <a href="http://Простокосмос.рф" target="_black">Just Space</a>
+		    </div>
+
+			<div class="social_network">
+				<h5>Мы в социальных сетях:</h5>
+				<a href="https://vk.com/spacedev"><img src="img/Vkontakte.svg" id="vk" class="social-icon vk" alt="vk.com" title="vk.com" onmouseover="over_social_icon('vk','Vkontakte-white')" onmouseout="out_social_icon('vk','Vkontakte')"></a>
+				<a href="#"><img src="img/twitter.svg" id="twitter" class="social-icon twitter" alt="twitter.com" title="twitter.com" onmouseover="over_social_icon('twitter','twitter-white')" onmouseout="out_social_icon('twitter','twitter')"></a>
+				<a href="#"><img src="img/facebook.svg" id="facebook" class="social-icon facebook" alt="facebook.com" title="facebook.com" onmouseover="over_social_icon('facebook','facebook-white')" onmouseout="out_social_icon('facebook','facebook')"></a>
+				<a href="#"><img src="img/YouTube.svg" id="YouTube" class="social-icon youtube" alt="YouTube.com" title="YouTube.com" onmouseover="over_social_icon('YouTube','YouTube-white')" onmouseout="out_social_icon('YouTube','YouTube')"></a>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ old_version/vacancies.php
@@ -0,0 +1,24 @@
+<?php
+	include("includes/functions.php");
+	include("includes/header.php");
+?>
+
+	<div class="content">
+		<div class="row">
+			<div class="block content-left">
+				<h2>Нам требуются:</h2>
+				<br>
+				<ul>
+					<li>Копирайтеры</li>
+					<li>SEO-оптимизаторы</li>
+					<li>Веб-дизайнеры</li>
+					<li>Front-end разработчики</li>
+					<li>Back-end разработчики</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+<?php
+	include("includes/footer.php");
+?>
