856decf859e04cfea4baca0729123521d1bf89e5
Анисимов Кирилл <k.anisimov@maximaster.ru>
2015-11-06 08:53:10 +0300

    Новый раздел сайта

---
+++ develop.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="ru">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="description" content="Мы создаем сайты под ключ: создание макета, программирование, продвижение. Разрабатываем сайты любой сложности, низкие цены и высокое качество" />
+		<meta name="keywords" content="создание сайтов, сайт визитка, разработка сайтов, создать интернет магазин, создание сайта визитки, продвижение сайтов, дизайн сайта, веб сайт, создать визитку, создание сайта цена, создать сайт быстро, раскрутка сайта, оптимизация сайта, разработка сайтов, seo продвижение, поисковое продвижение, поисковая оптимизация, продвинуть сайт, контекстная реклама, seo сайта, сео продвижение, seo продвижение сайта, продвижение сайта цена, продвижение веб сайтов, заказать продвижение сайта, стоимость продвижения сайта, качественное продвижение сайтов">
+		<meta http-equiv="content-language" content="ru">
+		<title>Just Space</title>
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/script.js"></script>
+	</head>
+	<body>
+		<nav>
+			<img src="img/JS.png" alt="Just Space" class="logo">
+			<ul class="menu">
+				<li><a href="index.php">Главная</a></li>
+				<li><a href="#landing">Лендинги</a></li>
+				<li><a href="#corp">Корпоративные</a></li>
+				<li><a href="#footer">Интернет-магазины</a></li>
+			</ul>
+		</nav>
+
+		<div><a id="landing"></a></div>
+		<div class="article services">
+			<h1>Лендинги</h1>
+			<table class="services-list">
+				<tr>
+					<td><img src="img/current.png" height="80" width="80"></td>
+					<td><img src="img/graph.png" height="80" width="80"></td>
+				</tr>
+				<tr>
+					<th>Что это такое?</th>
+					<th>Сроки/цена</th>
+				</tr>
+				<tr>
+					<td>Вам необходимо создать уникальный макет сайта или воспользоваться готовым, но подстроенным точно под вас решением? Тогда вы пришли по адресу.</td>
+					<td>Занимаемся созданием сайтов любой сложности: корпоративные сайты, сайты-визитки, интернет-магазины, лендинги и другие виды сайтов.</td>
+				</tr>
+			</table>	
+		</div>
+
+		<div class="marker">
+			<span>Почему у нас так дешево?</span>
+			Мы обеспечиваем хорошее качество, по низкой цене. Вы можете усомниться в этом, но все наши сотрудники работают удаленно,
+			благодаря этому наши производственные затраты малы и мы реализуем все проекты за минимальную цену.
+		</div>
+
+		<div><a id="corp"></a></div>
+		<div class="article services">
+			<h1>Корпоративные сайты</h1>
+			<table class="services-list">
+				<tr>
+					<td><img src="img/current.png" height="80" width="80"></td>
+					<td><img src="img/graph.png" height="80" width="80"></td>
+				</tr>
+				<tr>
+					<th>Что это такое?</th>
+					<th>Сроки/цена</th>
+				</tr>
+				<tr>
+					<td>Вам необходимо создать уникальный макет сайта или воспользоваться готовым, но подстроенным точно под вас решением? Тогда вы пришли по адресу.</td>
+					<td>Занимаемся созданием сайтов любой сложности: корпоративные сайты, сайты-визитки, интернет-магазины, лендинги и другие виды сайтов.</td>
+				</tr>
+			</table>	
+		</div>
+
+		<div class="marker social">
+			<div class="col">
+				<span>Мы в социальных сетях</span>
+				<div class="col-l">
+					<a href="https://vk.com/spacedev" class="social-icon" target="_blank"><img src="img/Vkontakte.svg" id="vk" alt="vk.com" title="vk.com" onmouseover="over_social_icon('vk','Vkontakte-white')" onmouseout="out_social_icon('vk','Vkontakte')"></a>
+				</div>
+				<div class="col-c">
+					<a href="https://twitter.com/Space_Develop" class="social-icon" target="_blank"><img src="img/twitter.svg" id="tw" alt="twitter.com" title="twitter.com" onmouseover="over_social_icon('tw','twitter-white')" onmouseout="out_social_icon('tw','twitter')"></a>
+				</div>
+				<div class="col-r">
+					
+					<a href="https://www.facebook.com/groups/931393063615918/" class="social-icon" target="_blank"><img src="img/facebook.svg" id="fb" alt="facebook.com" title="facebook.com" onmouseover="over_social_icon('fb','facebook-white')" onmouseout="out_social_icon('fb','facebook')"></a>
+				</div>
+			</div>
+		</div>
+
+		<a href="" id="head" class="top">^</a>
+	</body>
+</html>
